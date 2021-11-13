075e309e413b4a63524510cb2ea82e5c2612c1df
Thomas Fournier <thomas42600.f@gmail.com>
2021-11-13 20:28:05 +0100

    Réorganisation fichiers

--- navigation.py
+++ navigation.py
@@ -1,16 +1,3 @@
-import xml.etree.ElementTree as ET
-
-def GpxToArray(file):
-    trace=[]
-    tree = ET.parse(file)
-    root = tree.getroot()
-    #return("L'element racine : ", root.tag)
-    #return("Le premier enfant de la racine : ", root[1][1].tag)
-    for a in root[1][1]:
-        trace.append(a.attrib['lat'])
-        trace.append(a.attrib['lon'])
-    return trace
-
 def encadrement(trace,lat,lon):
     res = []
     cpt = 2
