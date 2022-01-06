d230bf157990f3293d4daa47fdb0d20fc183dcab
Thomas Fournier <thomas42600.f@gmail.com>
2022-01-06 13:52:33 +0100

    hgdghf

--- mainWithoutDB.py
+++ mainWithoutDB.py
@@ -6,7 +6,7 @@ from controller.useSensors import *
 """Initialisation of main parameters"""
 stop = True
 listeTrace=[1]
-file = ["/home/pi/Projet_ASAIoT/Examples_GPX/test2.gpx"]
+file = ["/home/pi/Projet_ASAIoT/Examples_GPX/campus.gpx"]
  
 """Initialisation of the objects"""
 nav2 = nav()
